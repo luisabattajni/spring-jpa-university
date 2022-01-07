eece5bb0dd010c5ff2039780297e2fa6479943aa
luisabattajni <luisasara.battajni@gmail.com>
2022-01-07 16:22:51 +0100

    wip

--- src/main/java/org/generation/italy/controller/DepartmentController.java
+++ src/main/java/org/generation/italy/controller/DepartmentController.java
@@ -1,11 +1,13 @@
 package org.generation.italy.controller;
 
+import org.generation.italy.model.Department;
 import org.generation.italy.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,6 +21,15 @@ public class DepartmentController {
 		model.addAttribute("departments", repository.findAll(Sort.by("name")));
 		return "departments";
 		
+	}
+	
+	@GetMapping("/department/{id}")
+	public String departmentDetail(Model model, @PathVariable Integer id) {
+		Department dep = repository.getById(id);
+		
+		model.addAttribute("department", dep);
+
+		return "detail";
 	}
 
 }
---
+++ src/main/java/org/generation/italy/model/Degree.java
@@ -0,0 +1,97 @@
+package org.generation.italy.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="degrees")
+public class Degree {
+	
+
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	// name level address email website (stringhe) + department_id
+	private String name;
+	private String level;
+	private String address;
+	private String email;
+	private String website;
+	
+	@ManyToOne //(fetch=FetchType.LAZY)
+	@JoinColumn(name="department_id")
+	private Department department;
+	
+	//get set
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+
+	
+	
+	
+
+}

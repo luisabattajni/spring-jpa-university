7f8bf6f03d17937af86f0b8b6e1145d02b74ebf1
luisabattajni <luisasara.battajni@gmail.com>
2022-01-07 16:55:12 +0100

    wip2

--- src/main/java/org/generation/italy/model/Degree.java
+++ src/main/java/org/generation/italy/model/Degree.java
@@ -2,6 +2,7 @@ package org.generation.italy.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -26,7 +27,7 @@ public class Degree {
 	private String email;
 	private String website;
 	
-	@ManyToOne //(fetch=FetchType.LAZY)
+	@ManyToOne (fetch=FetchType.EAGER)
 	@JoinColumn(name="department_id")
 	private Department department;
 	
